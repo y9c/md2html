97a79fa1d68d4c20aa6e554fad0516a2f34a6582
Ye Chang <yech1990@gmail.com>
2020-04-16 03:25:37 +0800

    bug: remove origin header

--- head2title_filter.py
+++ head2title_filter.py
@@ -12,12 +12,13 @@ Pandoc filter to convert all level 2+ headers to paragraphs with
 emphasized text.
 """
 
-from pandocfilters import toJSONFilter
+from pandocfilters import toJSONFilter, Null
 
 
 def behead(key, value, format, meta):
     if key == "Header" and value[0] == 1 and "title" not in meta:
         meta["title"] = {"t": "MetaInlines", "c": value[2]}
+        return Null()
 
 
 if __name__ == "__main__":
